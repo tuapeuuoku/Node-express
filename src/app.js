55a57eadbfa4fad293eba6adb6736df655f26974
tuapeuuoku <traperrokukurwa@wp.pl>
2023-09-13 14:23:25 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('views', './src/views');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', {})
+});
+
+app.get('omnie', (req, res) => {
+    res.render('omnie', {})
+});
+
+app.get('kontakt', (req, res) => {
+    res.render('kontakt', {
+        PageTitle: 'Strona Kontakt'
+    })
+});
+
+app.listen(5000, () => {
+    console.log("serwer dziala")
+});
+
